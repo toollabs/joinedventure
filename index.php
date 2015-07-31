fac1d9d417d82eb0659f92055a699402ee655802
Steinsplitter <steinsplitter-wiki@live.com>
2015-07-31 14:52:01 +0200

    simple index

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="de" http-equiv="Content-Language" />
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>joinedventure index</title>
+</head>
+
+<body>
+
+<h1>Index</h1>
+<ul>
+	<li><a href="/joinedventure/catmove.php">CatMove RC</a></li>
+</ul>
+<p><a href="https://github.com/Toollabs/joinedventure">Source</a></p>
+
+</body>
+
+</html>
